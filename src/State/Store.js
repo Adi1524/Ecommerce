f6bc6a8cf4fef2c995d8c3ed39b5c04af43b06b8
Aditya Kumar <adikumar8150@gmail.com>
2024-07-24 02:03:42 +0530

    redux is setup for order

--- src/State/Store.js
+++ src/State/Store.js
@@ -3,11 +3,13 @@ import { thunk } from "redux-thunk";
 import { authReducer } from "./Auth/Reducer";
 import { customerProductReducer } from "./Product/Reducer";
 import { customerCartReducer } from "./Cart/Reducer";
+import { customerOrderReducer } from "./Order/Reducer";
 
 const rootReducers = combineReducers({
   auth: authReducer,
   product: customerProductReducer,
   cart: customerCartReducer,
+  order: customerOrderReducer,
 });
 
 export const store = legacy_createStore(rootReducers, applyMiddleware(thunk));
